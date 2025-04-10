9f5b3437630aee45f9a75897b2a9fc77464dfa65
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-10 09:55:00 +0500

    Update

--- lib/Constants/FontWidgets.dart
+++ lib/Constants/FontWidgets.dart
@@ -127,10 +127,9 @@ class AppWidget {
     double fontSize =
         screenWidth > 1200
             ? 50
-            : // Large screens (desktop)
-            screenWidth > 600
+            : screenWidth > 600
             ? 40
-            : 30; // Tablets and phones
+            : 30;
     return GoogleFonts.playfairDisplay(
       textStyle: TextStyle(
         color: Colors.white,
